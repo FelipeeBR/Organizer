649b409da78decd82091277ccafd465f15c4f4f8
Felipe Mendes Neves <felipemen74@gmail.com>
2025-02-17 18:46:40 -0300

    atualização em notificações backend

--- backend/src/controllers/notificacaoController.js
+++ backend/src/controllers/notificacaoController.js
@@ -2,7 +2,25 @@ const { PrismaClient } = require("@prisma/client");
 const jwt = require('jsonwebtoken');
 const prisma = new PrismaClient();
 
-async function verificarNotificacoes() {
+async function enviarNotificacaoExpo(pushToken, mensagem, title) {
+    const message = {
+        to: pushToken,
+        sound: 'default',
+        title: title,
+        body: mensagem,
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(message),
+    });
+}
+
+/*async function verificarNotificacoes() {
     const tarefas = await prisma.tarefa.findMany({
         where: {
             date: {
@@ -55,6 +73,64 @@ async function verificarNotificacoes() {
             });
         }
     }
+}*/
+
+async function verificarNotificacoes() {
+    const tarefas = await prisma.tarefa.findMany({
+        where: { date: { lte: new Date() }, status: { not: 'COMPLETED' } },
+    });
+
+    const agendas = await prisma.agenda.findMany({
+        where: {
+            date: {
+                lte: new Date(),
+            },
+        },
+    });
+
+    for(const tarefa of tarefas) {
+        const existeNotificacao = await prisma.notificacao.findFirst({
+            where: { tarefaId: tarefa.id },
+        });
+
+        if(!existeNotificacao) {
+            const user = await prisma.usuario.findUnique({ where: { id: tarefa.userId } });
+            if(user?.expoPushToken) {
+                await enviarNotificacaoExpo(user.expoPushToken, `Tarefa: ${tarefa.title} - ${tarefa.description}`, tarefa.title);
+            }
+
+            await prisma.notificacao.create({
+                data: {
+                    descricao: `Tarefa: ${tarefa.title} - ${tarefa.description}`,
+                    userId: tarefa.userId,
+                    tarefaId: tarefa.id,
+                },
+            });
+        }
+    }
+
+    for(const agenda of agendas) {
+        const existeNotificacao = await prisma.notificacao.findFirst({
+            where: {
+                agendaId: agenda.id,
+            },
+        });
+
+        if(!existeNotificacao) {
+            const user = await prisma.usuario.findUnique({ where: { id: agenda.userId } });
+            if(user?.expoPushToken) {
+                await enviarNotificacaoExpo(user.expoPushToken, `Agenda: ${agenda.title} - ${agenda.description}`, agenda.title);
+            }
+
+            await prisma.notificacao.create({
+                data: {
+                    descricao: `Agenda: ${agenda.description}`,
+                    userId: agenda.userId,
+                    agendaId: agenda.id,
+                },
+            });
+        }
+    }
 }
 
 async function updateNotificacao(id) {
@@ -72,24 +148,6 @@ async function updateNotificacao(id) {
         return error;
     }
 };
-
-async function updateNotificacaoApp(id) {
-    try {
-        const notificacao = await prisma.notificacao.update({
-            where: {
-              id: parseInt(id),
-            },
-            data: {
-              agendada: true,
-            },
-        });
-        console.log(notificacao);
-        return notificacao;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
-}
 
 async function getNotificacoes(token) {
     try {
@@ -113,9 +171,31 @@ async function getNotificacoes(token) {
     }
 };
 
+async function salvarToken(expoToken, token) {
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+        const userId = decoded.id;
+        const user = await prisma.usuario.update({
+            where: {
+                id: userId
+            },
+            data: {
+                expoPushToken: expoToken
+            }
+        });
+        if(!user) {
+            return null;
+        }
+        return user;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
 module.exports = {
     verificarNotificacoes,
     updateNotificacao,
     getNotificacoes,
-    updateNotificacaoApp,
+    salvarToken,
 }
--- backend/src/routes/notificacaoRoutes.js
+++ backend/src/routes/notificacaoRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
-const { verificarNotificacoes, updateNotificacao, getNotificacoes, updateNotificacaoApp } = require("../controllers/notificacaoController");
+const { verificarNotificacoes, updateNotificacao, getNotificacoes, salvarToken } = require("../controllers/notificacaoController");
 
 const router = express.Router();
 
@@ -32,14 +32,25 @@ router.get('/notificacoes', auth, async (req, res) => {
     }
 });
 
-router.put('/notificacaoapp/:id', async (req, res) => {
-    const { id } = req.params;
+router.put('/salvar-token', auth, async (req, res) => {
+    const { expoToken, token } = req.body;
     try {
-        const notificacao = await updateNotificacaoApp(id);
-        res.status(200).json(notificacao); 
+        await salvarToken(expoToken, req.token);
+        res.status(200).json({ message: 'ExpoToken salvo com sucesso' });
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+router.put('/remover-token', auth, async (req, res) => {
+    const { token } = req.body;
+    try {
+        await salvarToken(null, req.token);
+        res.status(200).json({ message: 'ExpoToken removido com sucesso' });
     } catch (error) {
         res.sendStatus(500);
     }
-})
+});
 
 module.exports = router;
